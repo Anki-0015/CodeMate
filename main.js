e472825e2d9a0cd30d97555037d8b7d920cf59a0
Ankit <156998935+Anki-0015@users.noreply.github.com>
2025-05-20 18:43:53 +0530

    electron with nextjs setup

---
+++ main.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      contextIsolation: true,
+    }
+  });
+
+  win.loadURL('http://localhost:3000');
+}
+
+app.whenReady().then(createWindow);
